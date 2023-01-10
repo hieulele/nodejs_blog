2c2754225864b139ffc79018604124c073cf5e1e
hieulele <79618870+hieulele@users.noreply.github.com>
2023-01-10 15:29:16 +0700

    prettier

--- src/index.js
+++ src/index.js
@@ -1,39 +1,37 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
-const route = require('./routes');
+const route = require("./routes");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(
-    express.urlencoded({
-        extended: true,
-    }),
+  express.urlencoded({
+    extended: true,
+  })
 );
 app.use(express.json());
 
 // HTTP logger
-  app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 // Template engine
 app.engine(
-    'hbs',
+  "hbs",
   handlebars.engine({
-        extname: '.hbs',
-    }),
+    extname: ".hbs",
+  })
 );
 
-
-
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
 // Routes init
 route(app);
 
 app.listen(port, () =>
-    console.log(`Example app listen at http://localhost:${port}`),
+  console.log(`Example app listen at http://localhost:${port}`)
 );
