a1c6d1e1920b099e8cf86d6522278523840f5bcf
hieulele <79618870+hieulele@users.noreply.github.com>
2023-01-10 15:23:57 +0700

    prettier

--- src/index.js
+++ src/index.js
@@ -8,22 +8,31 @@ const port = 3000;
 const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 // HTTP logger
-app.use(morgan('combined'));
+  app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars.engine({
-    extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+  handlebars.engine({
+        extname: '.hbs',
+    }),
+);
+
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
 route(app);
 
-app.listen(port, () => console.log(`Example app listen at http://localhost:${port}`));
+app.listen(port, () =>
+    console.log(`Example app listen at http://localhost:${port}`),
+);
